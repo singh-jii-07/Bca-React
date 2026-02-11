e072954867b68ed3b1d40855fe89c538d2905c9b
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-02-11 10:39:17 +0530

    Day 2

---
+++ Day2/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Hero from './Components/Hero'
+import Section from './Components/Section'
+
+function App() {
+  
+
+  return (
+   <div>
+    <Hero/>
+    <Section/>
+    <Hero/>
+   </div>
+  )
+}
+
+export default App
---
+++ Day2/src/Components/Section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div>
+      <h1>
+        This is Somthing Section 
+      </h1>
+    </div>
+  )
+}
+
+export default Section
